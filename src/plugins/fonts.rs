3aee6793f7be52e3ebbad79de89d772422798d64
Miska Alexia <alexia.miska@gmail.com>
2025-07-15 15:53:32 -0700

    add Rotis font

--- src/plugins/fonts.rs
+++ src/plugins/fonts.rs
@@ -6,7 +6,7 @@ pub(crate) fn plugin(app: &mut App) {
     app.register_type::<FontAssets>();
 
     let assets = app.world().resource::<AssetServer>();
-    let default_font: Handle<Font> = assets.load("fonts/PixelSmall.ttf");
+    let default_font: Handle<Font> = assets.load("fonts/Rotis Serif Std/Rotis Serif Std Regular/Rotis Serif Std Regular.otf");
 
     app.insert_resource(FontAssets {
         default: default_font,
